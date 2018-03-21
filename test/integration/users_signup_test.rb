aa6c1b2c453528f0a7e782c38b01028f2e1d3a25
Yang Zheng <875333005@qq.com>
2018-03-22 05:12:28 +0800

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,13 +6,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
 	test "invalid signup information" do 
 		get signup_path
-		assert_no_difference 'User.count' do
+		assert_no_difference 'User.count', 1 do
       		post users_path, user: { name:  "",
                              email: "user@invalid",
                              password:        "foo",
 							 password_confirmation: "bar" }
 		end
-		assert_template 'users/new' 
+		assert_template 'users/new'
+		assert_select 'div#<CSS id for error explanation>'
+        assert_select 'div.<CSS class for field with error>'
+  		
 	end
 
 
@@ -28,5 +31,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 												password_confirmation: password }
 	end
 		assert_template 'users/show' 
+		# assert_not flash.FILL_IN
+		assert is_logged_in?
     end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,29 @@ require File.expand_path('../../config/environment', __FILE__)
 class ActiveSupport::TestCase
 # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical # order.
 	fixtures :all
-	include ApplicationHelper
+#	include ApplicationHelper
+
+
+	def is_logged_in?
+		!session[:user_id].nil? 
+	end
+
+
+	def log_in_as(user, options = {})
+		password = options[:password] || 'password' 
+		remember_me = options[:remember_me] || '1'
+		if integration_test?
+          post login_path, session: { email:       user.email,
+                                      password:    password,
+                                      remember_me: remember_me }
+		else
+			session[:user_id] = user.id 
+		end
+	end
+
+	def integration_test?
+		defined?(post_via_redirect)
+	end
+
       # Add more helper methods to be used by all tests here...
 end
